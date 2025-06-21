201d12535fe5c9c2ae393552b1ec9144a109bce1
santhosh v <santhoshv@sketchbrahma.com>
2025-06-21 12:55:21 +0530

    marquee tag content and structure changed

--- src/components/MarqueeCards/MarqueeCards.jsx
+++ src/components/MarqueeCards/MarqueeCards.jsx
@@ -1,44 +1,80 @@
 import "./marqueeCards.scss";
-import bed from "../../assets/images/marqueeCardsImages/bed.svg";
-import heart from "../../assets/images/marqueeCardsImages/heart.svg";
-import uro from "../../assets/images/marqueeCardsImages/uro.svg";
-import pain from "../../assets/images/marqueeCardsImages/pain.svg";
-import lab from "../../assets/images/marqueeCardsImages/lab.svg";
-import hemo from "../../assets/images/marqueeCardsImages/hemo.svg";
+// import bed from "../../assets/images/marqueeCardsImages/bed.svg";
+// import heart from "../../assets/images/marqueeCardsImages/heart.svg";
+// import uro from "../../assets/images/marqueeCardsImages/uro.svg";
+// import pain from "../../assets/images/marqueeCardsImages/pain.svg";
+// import lab from "../../assets/images/marqueeCardsImages/lab.svg";
+// import hemo from "../../assets/images/marqueeCardsImages/hemo.svg";
 import Marquee from "react-fast-marquee";
 
 const MarqueeCards = () => {
   const marqueeCardsData = [
     {
-      icon: bed,
-      title: "100-Bedded Facility",
-      content: "Spacious hospital designed for comprehensive patient care.",
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "Morethan 24 hours of excellence",
     },
     {
-      icon: heart,
-      title: "16 Hi-Tech ICU",
-      content: "Advanced intensive care for critical cases and emergencies.",
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "100 Bedded Multi Speciality Hospital",
     },
     {
-      icon: uro,
-      title: "Urodynamics",
-      content: "Specialized testing for urinary function and bladder health.",
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "Advanced Laser Laparoscopy Procedures",
     },
     {
-      icon: pain,
-      title: "Pain-Free Delivery",
-      content: "Modern, comfortable, and safe childbirth experience.",
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "Advanced Laser Endoscopic Procedures",
     },
     {
-      icon: lab,
-      title: "Laboratory Services",
-      content: "Accurate diagnostic testing with quick results.",
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "16 Bedded ICU",
     },
     {
-      icon: hemo,
-      title: "Hemodialysis",
-      content: "Expert renal care and dialysis support for kidney patients.",
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "Level 2 Nursery",
     },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "Modular Operation Theaters",
+    },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "Multi speciality OPD Services ",
+    },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "24 Hours Emergency Services",
+    },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "24 Hours Pharmacy Services",
+    },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "24 Hours Laboratory Services",
+    },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "24 Hours Ambulance Services",
+    },
+    {
+      // icon: bed,
+      // title: "100-Bedded Facility",
+      content: "24 Hours X-Ray & CT Scan",
+    },
+   
   ];
   return (
     <section className="marqueeCards">
@@ -51,11 +87,11 @@ const MarqueeCards = () => {
         >
           {marqueeCardsData.map((item, index) => (
             <div className="cardContainer" key={index}>
-              <div className="imageWrapper">
+              {/* <div className="imageWrapper">
                 <img src={item.icon} alt="icon" />
-              </div>
+              </div> */}
               <div className="textContainer">
-                <p className="cardTitle">{item.title}</p>
+                {/* <p className="cardTitle">{item.title}</p> */}
                 <p className="cardContent">{item.content}</p>
               </div>
             </div>
