b6b36910f730bc0b8a40b871b3ed271b7d5d9afb
santhosh v <santhoshv@sketchbrahma.com>
2025-07-01 03:32:54 +0530

    modal issue fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,64 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
+// import "./modal.scss";
+
+// const Modal = ({ isOpen = false, handleClose = () => {}, children }) => {
+//   const modalRef = useRef(null);
+
+//   useEffect(() => {
+//     document.body.style.overflow = isOpen ? "hidden" : "auto"; // Prevent scrolling when modal is open
+
+//     const handleOutsideClick = (e) => {
+//       if (modalRef.current && !modalRef.current.contains(e.target)) {
+//         handleClose();
+//       }
+//     };
+
+//     if (isOpen) {
+//       document.addEventListener("mousedown", handleOutsideClick);
+//     } else {
+//       document.removeEventListener("mousedown", handleOutsideClick);
+//     }
+
+//     return () => {
+//       document.body.style.overflow = "auto";
+//       document.removeEventListener("mousedown", handleOutsideClick);
+//     };
+//   }, [isOpen, handleClose]);
+
+//   if (!isOpen) return null;
+//   return (
+//     <div className="modal">
+//       <div
+//         className="modalContainer"
+//         ref={modalRef}
+//         onClick={(e) => {
+//           e.stopPropagation();
+//         }}
+//       >
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Modal;
+
+import React from "react";
 import "./modal.scss";
 
 const Modal = ({ isOpen = false, handleClose = () => {}, children }) => {
-  const modalRef = useRef(null);
-
-  useEffect(() => {
-    document.body.style.overflow = isOpen ? "hidden" : "auto"; // Prevent scrolling when modal is open
-
-    const handleOutsideClick = (e) => {
-      if (modalRef.current && !modalRef.current.contains(e.target)) {
-        handleClose();
-      }
-    };
-
-    if (isOpen) {
-      document.addEventListener("mousedown", handleOutsideClick);
-    } else {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    }
-
+ 
+// Prevent background scroll
+  React.useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "auto";
     return () => {
       document.body.style.overflow = "auto";
-      document.removeEventListener("mousedown", handleOutsideClick);
     };
-  }, [isOpen, handleClose]);
+  }, [isOpen]);
 
   if (!isOpen) return null;
   return (
-    <div
-      className="modal"
-      ref={modalRef}
-     
-    >
+    <div className="modal"  onClick={handleClose} >
       <div
         className="modalContainer"
         onClick={(e) => {
@@ -45,3 +72,4 @@ const Modal = ({ isOpen = false, handleClose = () => {}, children }) => {
 };
 
 export default Modal;
+
--- src/containers/BookAppointment/BookAppointment.jsx
+++ src/containers/BookAppointment/BookAppointment.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import AppointmentWrapper from "../../components/AppointmentWrapper/AppointmentWrapper";
 import ContactUs from "../../components/ContactUs/ContactUs";
 import "./bookAppointment.scss";
 const BookAppointment = () => {
+      useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }, []);
   return (
     <>
       <div className="bookAppointmentPage">
